1ceae8b5e08e3ce7fc30fbd505205133ed0a77a5
Essei Takada <essei@EsseinoMacBook-Pro.local>
2022-05-03 12:35:47 +0900

    通知機能 backend実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,7 @@
 class LikesController < ApplicationController
   def create
-    @like = current_user.likes.create(post_id: params[:post_id])
+    @like = current_user.likes.create(micropost_id: params[:micropost_id])
+    create_like_notification!(current_user)
     redirect_back(fallback_location: root_path)
   end
 
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -2,6 +2,7 @@ class Reply < ApplicationRecord
   belongs_to :user
   belongs_to :micropost
   has_many :replies_to_reply, class_name: "Reply", foreign_key: :reply_id, dependent: :destroy
+  has_many :notifications, dependent: :destroy
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :micropost_id, presence: true
